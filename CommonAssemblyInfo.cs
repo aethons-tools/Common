84636d42020af5bbb80e41c593cfacdc8ab80136
brent mccullough <brent@aethon.io>
2016-05-05 12:19:23 -0500

    moved build files here and added common assembly info

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Aethon's Tools")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
